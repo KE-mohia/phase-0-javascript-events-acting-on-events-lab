4d6072e5fc9289ac472b526a53625e308ac51c2a
wayne <keyawayne1@gmail.com>
2023-06-24 00:25:58 +0300

    almost done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+function moveDodgerLeft(){
+    var leftNumber = dodger.style.left.replace("px","");
+    var left = parseint(leftNumbers, 10);
+
+    if (left > 0) {
+        dodger.style.left = left - 10 + "px";
+    }
+
+}
+function moveDodgerRight(){
+    var leftNumber = dodger.style.left.replace("px","");
+    var left = parseInt(leftNumbers, 10);
+
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "Arrowleft"){
+        moveDodgerLeft();
+    }
+});
+
+document.addEventListener("keydown",(e) => {
+    if (e.key === "ArrowRight"){
+        moveDodgerRight();
+    }
+});
